4674e8b03d760ca5034ffc006aea38a9d68e2f0b
Horimoto Yasuhiro <horimoto@clear-code.com>
2026-07-15 18:06:34 +0900

    Debug

--- data/rpm/post.sh
+++ data/rpm/post.sh
@@ -80,7 +80,7 @@ if [ "${try_auto_prepare}" = "yes" ]; then
          -u root \
          --connect-expired-password \
          -p"${auto_generated_password}" \
-         -e "quit" > /dev/null 2>&1; then
+         -e "quit"; then
       if "${mysql_command}" \
            -u root \
            --connect-expired-password \
